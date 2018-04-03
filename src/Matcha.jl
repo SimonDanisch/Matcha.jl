2c9bdd0ca5eb3c1ae5cf7d53468298f5fdb914b9
SimonDanisch <sdanisch@gmail.com>
2018-04-03 16:35:44 +0200

    changes for sugar

--- src/Matcha.jl
+++ src/Matcha.jl
@@ -15,9 +15,11 @@ Greed(x::F) where {F} = Greed(x, 1:typemax(Int))
 greediness(x) = 1:1
 greediness(x::Greed) = x.range
 
+abstract type MatchFunc end
+
 
 trymatch(ms::MatchSteering, val, history) = trymatch(ms.x, val, history)
-function trymatch(f::Function, val, history)
+function trymatch(f::Union{Function, MatchFunc}, val, history)
     # an atomic pattern can use the match history by having two arguments
     if applicable(f, val, history)
         f(val, history)
@@ -32,8 +34,11 @@ struct History{T, VT, ST}
     buffer::T # optional record of elements for iterators that are volatile. If not volatile, this will be the actual iterator
     matches::Vector{VT} # flattened list of views for each sub pattern match
     last_begin::Ref{ST} # state of last pattern match begin
+    env::Dict{Symbol, Any} # environment for more complex saving of matched variables
 end
 @inline Base.getindex(h::History, i::Integer) = h.matches[i]
+@inline Base.getindex(h::History, key::Symbol) = h.env[key]
+@inline Base.haskey(h::History, key::Symbol) = haskey(h.env, key)
 
 # trait system
 Base.@pure needs_recording(x) = false
@@ -47,7 +52,8 @@ function History(list, state)
     History(
         buffer_type(list),
         view_type(list),
-        Ref(state)
+        Ref(state),
+        Dict{Symbol, Any}()
     )
 end
 
@@ -74,17 +80,22 @@ _copy(x::String) = x
 _copy(x::Ref) = Ref(x[])
 
 function Base.copy(h::History)
-    History(_copy(h.buffer), _copy(h.matches), _copy(h.last_begin))
+    History(_copy(h.buffer), _copy(h.matches), _copy(h.last_begin), deepcopy(h.env))
 end
-function view_constructor(h::History{X, T, Y}, a, b) where {X, Y, T <: SubArray}
+function view_constructor(pattern::Greed, h::History{X, T, Y}, a, b) where {X, Y, T <: SubArray}
+    view_constructor(pattern.x, h, a, b)
+end
+function view_constructor(pattern, h::History{X, T, Y}, a, b) where {X, Y, T <: SubArray}
     view(h.buffer, a:b)
 end
-function view_constructor(h::History{X, T, Y}, a, b) where {X, Y, T <: SubString}    
+function view_constructor(pattern, h::History{X, T, Y}, a, b) where {X, Y, T <: SubString}
     safe_substring(h.buffer, a, b)
 end
-function finish_match(matched, h::History{T, VT, ST}, state) where {T, VT, ST}
+function finish_match(pattern, matched, h::History{T, VT, ST}, state) where {T, VT, ST}
     if matched
-        push!(h.matches, view_constructor(h, h.last_begin[], state))
+        push!(h.matches, view_constructor(pattern, h, h.last_begin[], state))
+    else
+        empty!(h.env)
     end
     matched, h, state
 end
@@ -116,14 +127,14 @@ function inner_matchat(
         else
             # we don't have enough matches yet to fail matching, or we don't have any more patterns to match
             if !enough || N == 1
-                return finish_match(enough, history, enough ? lastmatchstate : state)..., matches # we fail or not, depending whether we have enough
+                return finish_match(pattern, enough, history, enough ? lastmatchstate : state)..., matches # we fail or not, depending whether we have enough
             end
             if N > 1
                 # okay, we failed but already have enough.
                 # The only chance to continue is that next pattern matches
                 # this is final, so no copy of history for backtracking needed
                 if matches > 0
-                    finish_match(true, history, lastmatchstate)
+                    finish_match(pattern, true, history, lastmatchstate)
                 end
                 return inner_matchat(list, last_state, tail(patterns), history)
             end
@@ -135,17 +146,17 @@ function inner_matchat(
             # we're in a state were the current pattern can/should stop matching
             # this is where a match of the next pattern could end things!
             if matches == last(greedrange) # we actually are at the last allowed
-                finish_match(true, history, lastmatchstate)
+                finish_match(pattern, true, history, lastmatchstate)
                 return inner_matchat(list, state, tail(patterns), history)
             else
                 # a copy of history is needed, since we can backtrack
                 newbranch = copy(history)
-                finish_match(true, newbranch, lastmatchstate - 1)
+                finish_match(pattern, true, newbranch, lastmatchstate - 1)
                 ismatch, history2, state2, n = inner_matchat(list, last_state, tail(patterns), newbranch)
                 (ismatch && n > 0) && return true, history2, state2, matches
             end
         elseif N == 1 && matches == last(greedrange) # rest is empty and we have enough -> stop!
-            res = finish_match(true, history, lastmatchstate)
+            res = finish_match(pattern, true, history, lastmatchstate)
             return res..., matches
         end
 
@@ -155,7 +166,7 @@ function inner_matchat(
             # this madness is over!
             # if succesfull or not depends on whether we have enough and no rest!
             success = enough && (N == 1 || all(x-> 0 in greediness(x), tail(patterns)))
-            return finish_match(success, history, success ? lastmatchstate : state)..., matches
+            return finish_match(pattern, success, history, success ? lastmatchstate : state)..., matches
         end
         # okay, we're here, meaning we matched in some way and can continue making history!
         last_state = state;
@@ -176,7 +187,7 @@ function matchat(
     matched, hist, state, n = inner_matchat(
         list, state, patterns, history
     )
-    matched, hist.matches
+    matched, hist
 end
 
 function matchone(
@@ -208,12 +219,12 @@ function matchitall(
         list, state, patterns::Tuple,
     )
     history = History(list, state)
-    matches = typeof(history.matches)[]
+    matches = typeof(history)[]
     while !done(list, state)
         history = History(list, state)
         match, history, _, n = inner_matchat(list, state, patterns, history)
         if match
-            push!(matches, history.matches)
+            push!(matches, history)
         end
         elem, state = next(list, state)
     end
@@ -234,6 +245,9 @@ if VERSION < v"0.7.0-DEV.3020"
     parentindices(v::SubArray) = v.indexes
 end
 
+@inline firstindex(v::History) = firstindex(v.matches)
+@inline lastindex(v::History) = lastindex(v.matches)
+
 @inline firstindex(v::SubArray) = parentindices(v)[1][1]
 @inline firstindex(v::Union{Vector, Tuple}) = firstindex(first(v))
 
@@ -252,9 +266,9 @@ function matchreplace(f, list, patterns)
         isreplace = i <= length(matches) && last_state == firstindex(cmatch)
         replacements, n = if isreplace
             i += 1
-            n = sum(map(slength, cmatch))
+            n = sum(map(slength, cmatch.matches))
             elem, state = forward(list, elem, state, n - 1)
-            tmp = f(cmatch...)
+            tmp = f(cmatch)
             r = isa(tmp, Tuple) ? tmp : (tmp,)
             if i <= length(matches)
                 cmatch = matches[i]
@@ -277,12 +291,12 @@ function matchreplace(f, list::AbstractVector, patterns)
     lastidx = 1
     for cmatch in matches
         a, b = firstindex(cmatch), lastindex(cmatch)
-        N =  sum(map(slength, cmatch))
+        N =  sum(map(slength, cmatch.matches))
         @assert (b - a) == (N - 1) "$a $b $N"
         a < lastidx && continue # ignore matches that go back
         append!(result, view(list, lastidx:a-1))
         lastidx = b + 1
-        tmp = f(cmatch...)
+        tmp = f(cmatch)
         r = isa(tmp, Tuple) ? tmp : (tmp,)
         push!(result, r...)
     end
